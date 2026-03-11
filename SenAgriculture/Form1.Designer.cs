faa5694961cb3eafdc1b9b2644db44361dc85a45
CMT SY <gat0sy>
2026-03-11 11:11:28 +0000

    feat: Design de l'écran d'accueil

--- SenAgriculture/Form1.Designer.cs
+++ SenAgriculture/Form1.Designer.cs
@@ -28,81 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.gradientPanel1 = new AppSenAgriculture.Helpers.GradientPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnQuitter = new System.Windows.Forms.Button();
             this.txtIdentifiant = new System.Windows.Forms.TextBox();
+            this.btnSeConnecter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.txtMotDePasse = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.btnSeConnecter = new System.Windows.Forms.Button();
-            this.btnQuitter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.gradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::AppSenAgriculture.Properties.Resources.Logo_Senagriculture___Alpha;
+            this.pictureBox2.Location = new System.Drawing.Point(316, 43);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(227, 225);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Reem Kufi", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label4.Location = new System.Drawing.Point(318, 244);
+            this.label4.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 49);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Sen";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Reem Kufi", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label5.Location = new System.Drawing.Point(377, 244);
+            this.label5.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 49);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Agriculture";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gradientPanel1
+            // 
+            this.gradientPanel1.Controls.Add(this.label3);
+            this.gradientPanel1.Controls.Add(this.pictureBox1);
+            this.gradientPanel1.Controls.Add(this.btnQuitter);
+            this.gradientPanel1.Controls.Add(this.txtIdentifiant);
+            this.gradientPanel1.Controls.Add(this.btnSeConnecter);
+            this.gradientPanel1.Controls.Add(this.label1);
+            this.gradientPanel1.Controls.Add(this.txtMotDePasse);
+            this.gradientPanel1.Controls.Add(this.label2);
+            this.gradientPanel1.gradientBottom = System.Drawing.Color.MediumSeaGreen;
+            this.gradientPanel1.gradientTop = System.Drawing.Color.LimeGreen;
+            this.gradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.gradientPanel1.Name = "gradientPanel1";
+            this.gradientPanel1.Size = new System.Drawing.Size(310, 357);
+            this.gradientPanel1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.MintCream;
+            this.label3.Location = new System.Drawing.Point(95, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 33);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Login";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::AppSenAgriculture.Properties.Resources.module_clouds_2x_Vertical___reversed1;
+            this.pictureBox1.Location = new System.Drawing.Point(252, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 380);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.BackColor = System.Drawing.Color.Crimson;
+            this.btnQuitter.Font = new System.Drawing.Font("Rubik", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuitter.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnQuitter.Location = new System.Drawing.Point(12, 266);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(114, 23);
+            this.btnQuitter.TabIndex = 4;
+            this.btnQuitter.Text = "&Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = false;
+            this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
+            // 
+            // txtIdentifiant
+            // 
+            this.txtIdentifiant.Location = new System.Drawing.Point(16, 142);
+            this.txtIdentifiant.Name = "txtIdentifiant";
+            this.txtIdentifiant.Size = new System.Drawing.Size(234, 20);
+            this.txtIdentifiant.TabIndex = 1;
+            // 
+            // btnSeConnecter
+            // 
+            this.btnSeConnecter.BackColor = System.Drawing.Color.Azure;
+            this.btnSeConnecter.Font = new System.Drawing.Font("Rubik", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSeConnecter.Location = new System.Drawing.Point(132, 266);
+            this.btnSeConnecter.Name = "btnSeConnecter";
+            this.btnSeConnecter.Size = new System.Drawing.Size(114, 23);
+            this.btnSeConnecter.TabIndex = 3;
+            this.btnSeConnecter.Text = "&Se connecter";
+            this.btnSeConnecter.UseVisualStyleBackColor = false;
+            this.btnSeConnecter.Click += new System.EventHandler(this.btnSeConnecter_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Reem Kufi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MintCream;
+            this.label1.Location = new System.Drawing.Point(16, 117);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.Size = new System.Drawing.Size(88, 27);
             this.label1.TabIndex = 0;
             this.label1.Text = "Identifiant";
             // 
-            // txtIdentifiant
-            // 
-            this.txtIdentifiant.Location = new System.Drawing.Point(12, 50);
-            this.txtIdentifiant.Name = "txtIdentifiant";
-            this.txtIdentifiant.Size = new System.Drawing.Size(331, 20);
-            this.txtIdentifiant.TabIndex = 1;
-            // 
             // txtMotDePasse
             // 
-            this.txtMotDePasse.Location = new System.Drawing.Point(12, 121);
+            this.txtMotDePasse.Location = new System.Drawing.Point(16, 213);
             this.txtMotDePasse.Name = "txtMotDePasse";
-            this.txtMotDePasse.Size = new System.Drawing.Size(331, 20);
+            this.txtMotDePasse.Size = new System.Drawing.Size(234, 20);
             this.txtMotDePasse.TabIndex = 2;
             this.txtMotDePasse.UseSystemPasswordChar = true;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 96);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Reem Kufi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MintCream;
+            this.label2.Location = new System.Drawing.Point(16, 188);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.Size = new System.Drawing.Size(103, 27);
             this.label2.TabIndex = 2;
             this.label2.Text = "Mot de Passe";
-            // 
-            // btnSeConnecter
-            // 
-            this.btnSeConnecter.Location = new System.Drawing.Point(229, 180);
-            this.btnSeConnecter.Name = "btnSeConnecter";
-            this.btnSeConnecter.Size = new System.Drawing.Size(114, 23);
-            this.btnSeConnecter.TabIndex = 3;
-            this.btnSeConnecter.Text = "&Se connecter";
-            this.btnSeConnecter.UseVisualStyleBackColor = true;
-            this.btnSeConnecter.Click += new System.EventHandler(this.btnSeConnecter_Click);
-            // 
-            // btnQuitter
-            // 
-            this.btnQuitter.Location = new System.Drawing.Point(12, 180);
-            this.btnQuitter.Name = "btnQuitter";
-            this.btnQuitter.Size = new System.Drawing.Size(114, 23);
-            this.btnQuitter.TabIndex = 4;
-            this.btnQuitter.Text = "&Quitter";
-            this.btnQuitter.UseVisualStyleBackColor = true;
-            this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
             // 
             // frmConnexion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(378, 262);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(557, 351);
             this.ControlBox = false;
-            this.Controls.Add(this.btnQuitter);
-            this.Controls.Add(this.btnSeConnecter);
-            this.Controls.Add(this.txtMotDePasse);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtIdentifiant);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.gradientPanel1);
             this.Name = "frmConnexion";
             this.Text = "Sen Agriculture :: Connexion";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.gradientPanel1.ResumeLayout(false);
+            this.gradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,6 +218,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnSeConnecter;
         private System.Windows.Forms.Button btnQuitter;
+        private Helpers.GradientPanel gradientPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
---
+++ SenAgriculture/Helpers/GradientPanel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace AppSenAgriculture.Helpers
+{
+    internal class GradientPanel : Panel
+    {
+        // Creer une propriété qui defini les coulours pour le dégradé de haut en bas
+
+        public Color gradientTop { get; set; }
+        public Color gradientBottom { get; set; }
+
+        // Creer un constructeur pour la classe GradientPanel
+        public GradientPanel() {
+            // rend le design adaptable au changement de taille.
+            this.Resize += GradientPanel_Resize;
+        }
+
+        private void GradientPanel_Resize(object sender, EventArgs e)
+        {
+            this.Invalidate(); //Force un une reconstruction au niveau graphique
+        }
+        // Écrase la méthode onPaint pour redessiner le degradé d'arrière plan
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // creer un lineargradient brush avec des valeurs 'top' et 'bottom' pour le degardé de couleur
+
+            LinearGradientBrush linear = new LinearGradientBrush(
+                this.ClientRectangle, // Zone à remplir avec le dégradé
+                this.gradientTop, // couleur de départ du gradient ( depuis le haut )
+                this.gradientBottom, // couleur d'arrivée du gradient ( vers le bas )
+                90F // l'angle du dégradé en degrées, ( par example ici 90 ~ vertical )
+                );
+
+            // Recupère le contexte graphique pour le dressage.
+            Graphics g = e.Graphics;
+
+            // Remplie toute la zone de l'élément avec le dégradé
+            g.FillRectangle(linear, this.ClientRectangle);
+
+            // Fait appel de la classe de base onPaint pour s'assurer de la réalisation de tout dressage additionnel
+            base.OnPaint(e);
+        }
+    } 
+}
